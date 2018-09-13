ca89cf2111e18a0606b98b6c894f7ad58b58ce6a
huangkai <huangkai18@gmail.com>
2018-09-13 15:25:37 +0800

    限流器

---
+++ src/test/java/com/hk/ssm4/TestRateLimiter.java
@@ -0,0 +1,38 @@
+package com.hk.ssm4;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.RateLimiter;
+
+public class TestRateLimiter {
+	public static void main(String[] args) {
+
+        //新建一个每秒限制3个的令牌桶
+        RateLimiter rateLimiter = RateLimiter.create(1.0);
+
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 10; i++) {
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    //获取令牌桶中一个令牌，最多等待10秒
+                    if (rateLimiter.tryAcquire(1, 10, TimeUnit.SECONDS)) {
+                        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+                    }
+                }
+            });
+        }
+
+        executor.shutdown();
+    }
+    
+}
