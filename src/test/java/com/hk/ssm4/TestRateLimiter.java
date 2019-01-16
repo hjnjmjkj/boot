bb088eb7e159f5ae092b5818857ccad080895bba
huangkai <huangkai18@gmail.com>
2019-01-16 16:24:57 +0800

    5

--- src/test/java/com/hk/ssm4/TestRateLimiter.java
+++ src/test/java/com/hk/ssm4/TestRateLimiter.java
@@ -37,6 +37,7 @@ public class TestRateLimiter {
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
+        System.out.println("5");
     }
     
 }
